260653098a2085129927092404bb38715979aa0a
Mohammed Kabli <76818640+mohammedkabli@users.noreply.github.com>
2022-02-18 11:18:55 +0100

    🎉 finich the project

--- test/test.js
+++ test/test.js
@@ -1,30 +1,4 @@
-// const calc = require ( '../index.js' )
-// const assert = require ('assert')
 
-// console.log(calc);
-
-// describe('calculator', function() {
-//   it('should add tow numbers', function(done) {
-//     const result = calc.add(5,2);
-//     assert.equal(result, 7);
-//     done();
-//   }),
-//   it('should add tow numbers', function(done) {
-//     const result = calc.subtract(5,2);
-//     assert.equal(result, 3);
-//     done();
-//   }),
-//   it('should add tow numbers', function(done) {
-//     const result = calc.divide(5,2);
-//     assert.equal(result, 2.5);
-//     done();
-//   }),
-//   it('should add tow numbers', function(done) {
-//     const result = calc.multiply(5,2);
-//     assert.equal(result, 10);
-//     done();
-//   })
-// })
 
 var calc = require('../index.js')
 var assert = require('assert')
